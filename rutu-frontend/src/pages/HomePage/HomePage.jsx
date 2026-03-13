b749c69dae193f22a3abde4920d7a08313ae2502
adyvka31 <rafifdyvka07@gmail.com>
2026-03-13 23:51:12 +0700

    feat: flow section design

--- rutu-frontend/src/pages/HomePage/HomePage.jsx
+++ rutu-frontend/src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import Hero from "@/pages/HomePage/Hero/Hero";
 import Marquee from "@/pages/HomePage/Marquee/Marquee";
 import StackCard from "@/pages/HomePage/StackCard/StackCard";
 import CalendarSection from "@/pages/HomePage/CalendarSection/CalendarSection";
+import FlowSection from "@/pages/HomePage/FlowSection/FlowSection";
 import MainLayout from "@/layouts/MainLayout/MainLayout";
 
 export default function HomePage() {
@@ -15,6 +16,8 @@ export default function HomePage() {
       <StackCard />
 
       <CalendarSection />
+
+      <FlowSection />
     </MainLayout>
   );
 }
