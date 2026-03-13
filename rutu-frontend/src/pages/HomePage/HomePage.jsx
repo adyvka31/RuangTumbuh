7eefca10ba977f7736a23ee3f0011f4bdfd3d6f9
Faznug_Space <153061199+FaishalFaiz@users.noreply.github.com>
2026-03-13 14:31:16 +0700

    refactor: folder structure

--- rutu-frontend/src/pages/HomePage/HomePage.jsx
+++ rutu-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import HeroLayout from "@/pages/HomePage/HeroLayout/HeroLayout";
-import MarqueeLayout from "@/pages/HomePage/MarqueeLayout/MarqueeLayout";
-import StackLayout from "@/pages/HomePage/StackLayout/StackLayout";
+import HeroLayout from "@/pages/HomePage/Hero/HeroLayout";
+import MarqueeLayout from "@/pages/HomePage/Marquee/MarqueeLayout";
+import StackLayout from "@/pages/HomePage/StackCard/StackLayout";
 import styles from "./HomePage.module.css";
-import MainLayout from "@/pages/HomePage/MainLayout/MainLayout";
+import MainLayout from "@/layouts/MainLayout/MainLayout";
 
 export default function HomePage() {
   return (
