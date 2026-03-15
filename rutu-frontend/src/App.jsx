4f186c33f3e2c5cf9748e0a14ebf5c5a6f269457
Faznug_Space <153061199+FaishalFaiz@users.noreply.github.com>
2026-03-15 16:07:26 +0700

    feat: Main dashboard template

--- rutu-frontend/src/App.jsx
+++ rutu-frontend/src/App.jsx
@@ -4,6 +4,10 @@ import { ReactLenis } from 'lenis/react'
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/LoginPage/LoginPage";
 import Register from "./pages/RegisterPage/RegisterPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import BookmarksPage from "./pages/BookmarksPage/BookmarksPage";
+import MessagesPage from "./pages/MessagesPage/MessagesPage";
 import CustomCursor from "./components/CustomCursor/CustomCursor";
 
 export default function App() {
@@ -15,6 +19,10 @@ export default function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/bookmarks" element={<BookmarksPage />} />
+          <Route path="/messages" element={<MessagesPage />} />
         </Routes>
       </BrowserRouter>
     </ReactLenis>
