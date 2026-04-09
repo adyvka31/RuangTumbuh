ccba78222e2f59612d3d420a006688573b61fcca
adyvka31 <rafifdyvka07@gmail.com>
2026-04-09 17:46:09 +0700

    delete auth.schema.mjs

--- packages/shared/validations/auth.schema.mjs
+++
@@ -1,8 +0,0 @@
-import * as authSchemas from "./auth.schema.js";
-
-// Ambil skema dengan aman, entah Rollup membungkusnya di .default atau langsung
-const schemas = authSchemas.default || authSchemas;
-
-// Export ulang secara Named Exports untuk Frontend
-export const loginPayloadSchema = schemas.loginPayloadSchema;
-export const registerPayloadSchema = schemas.registerPayloadSchema;
